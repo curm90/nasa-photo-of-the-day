89a25c9d7bc73b6f86799a4277272b519fd25d05
Liam Sutton <devliam90@gmail.com>
2019-09-11 12:20:01 +0100

    add api request and log data

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
-import React from "react";
-import "./App.css";
+import React, { useEffect } from 'react';
+import './App.css';
+import ReactPlayer from 'react-player';
+
+const axios = require('axios');
+
+const apiKey = '0T7g7WYSUw4zezlzMYFTNDgOYldC5fpY3dGvix03';
 
 function App() {
-  return (
-    <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
-    </div>
-  );
+  // const [mediaUrl, setImage] = useState('');
+  // const [mediaType, setMediaType] = useState('');
+  // const [title, setTitle] = useState('');
+  // const [explanation, setExplanation] = useState('');
+  // const [date, setDate] = useState('');
+  // const [dateQuery, setDateQuery] = useState('2019-02-14');
+
+  useEffect(() => {
+    axios
+      .get(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}`)
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, []);
+
+  return <div className="App"></div>;
 }
 
 export default App;
